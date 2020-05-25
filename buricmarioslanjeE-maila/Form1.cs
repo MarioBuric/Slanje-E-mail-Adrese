f48e2e964d97007c3ff12e27a1611197fffd2bac
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2020-05-25 20:28:36 +0200

    Add files via upload

---
+++ buricmarioslanjeE-maila/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buricmarioslanjeE_maila
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("marioburic39@gmail.com");
+                mail.To.Add("marioburic39@gmail.com");
+                mail.Subject = "Test Mail";
+                mail.Body = "This is for testing SMTP mail from GMAIL";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("marioburic39@gmail.com", "");
+                SmtpServer.EnableSsl = true;
+
+                MessageBox.Show("mail poslan");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
